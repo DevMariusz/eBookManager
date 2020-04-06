0adaf07f3cd05176cb82a00974ce237b35bdf8c0
Mariusz Przybysz <Devmariusz.h@gmail.com>
2020-04-06 07:43:24 -0600

    Project Setup

---
+++ eBookManager.Engine/DeweyDecimal.cs
@@ -0,0 +1,9 @@
+﻿namespace eBookManager.Engine
+{
+    public class DeweyDecimal
+    {
+        public string ComputerScience = "000";
+        public string DataProcessing = "004";
+        public string ComputerProgramming = "005";
+    }
+}
---
+++ eBookManager.Engine/Document.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace eBookManager.Engine
+{
+    public class Document
+    {
+        public string Title;
+        public DeweyDecimal Classification;
+    }
+}
