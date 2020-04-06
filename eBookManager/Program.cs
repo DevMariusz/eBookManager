3eca287e472c1e04de3bfd9713c29b0fa508dee8
Mariusz Przybysz <Devmariusz.h@gmail.com>
2020-04-06 08:08:01 -0600

    Basic file structure setup

--- eBookManager/Program.cs
+++ eBookManager/Program.cs
@@ -18,6 +18,8 @@ namespace eBookManager
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            
         }
     }
 }
