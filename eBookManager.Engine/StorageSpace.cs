b7c21eb244e48cb622438b40addcfd65e7c141ff
Mariusz Przybysz <Devmariusz.h@gmail.com>
2020-04-08 07:36:25 -0600

    Helper Methods

---
+++ eBookManager.Engine/StorageSpace.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eBookManager.Engine
+{
+    public class StorageSpace
+    {
+        public int ID;
+        public string Name;
+
+        public List<Document> BookList;
+    }
+}
--- eBookManager.Helper/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace eBookManager.Helper
-{
-    public class Class1
-    {
-    }
-}
